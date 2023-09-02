2e23eb9c9f070ad2e89a8b0e993f634f6bc0e978
noel-hsiao <143864973+noel-hsiao@users.noreply.github.com>
2023-09-02 21:10:08 +0800

    add model

---
+++ Model/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebAPI.Model
+{
+    public class Account
+    {
+        string User;
+        string Password;
+        DateTime CreateTime;
+    }
+}
---
+++ Model/ChatBoard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebAPI.Model
+{
+    public class ChatBoard
+    {
+        string Title;
+        string Content;
+        bool IsDelete;
+        DateTime CreateTime;
+        DateTime UpdateTime;
+    }
+}
